049c633ef9c343c21b1297132aac9f5b8d955e80
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-10-25 22:34:15 +0300

    fix(react-app); fix event message

--- react-app/src/app/app.tsx
+++ react-app/src/app/app.tsx
@@ -10,7 +10,7 @@ export const App = () => {
 
     window.appEventTarget.dispatchEvent(
       new CustomEvent('message-to-solid', {
-        detail: message || 'Пустое сообщение',
+        detail: message,
       }),
     );
   };
