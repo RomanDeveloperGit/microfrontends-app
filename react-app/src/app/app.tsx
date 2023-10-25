d318023eeaf42644c2d08498f5fdecb2aed1b0aa
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-10-25 22:03:37 +0300

    feat(react-app): add message sender to Solid App

--- react-app/src/app/app.tsx
+++ react-app/src/app/app.tsx
@@ -1,9 +1,24 @@
 import { EventsBox } from '../events-box';
 
 export const App = () => {
+  const handleSendMessage = () => {
+    const message = prompt(
+      'Введите текст сообщения, которые вы хотите отправить в приложение Solid JS',
+    );
+
+    if (message === null) return;
+
+    window.appEventTarget.dispatchEvent(
+      new CustomEvent('message-to-solid', {
+        detail: message || 'Пустое сообщение',
+      }),
+    );
+  };
+
   return (
     <div>
       <h1>React App</h1>
+      <button onClick={handleSendMessage}>Отправить сообщение в Solid JS приложение</button>
       <EventsBox />
     </div>
   );
